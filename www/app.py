6b94d93ee898e92717a38cd5381068f77fa8a697
mac <mac@macdeMacBook-Pro.local>
2016-10-13 22:39:04 +0800

    add app.py

---
+++ www/app.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__= 'Yilin Liu'
+
+'''
+async web application
+'''
+
+import logging; logging=basicConfig(level=logging.INFO)
+
+import asyncio, os, json, time
+from datetime import datetime
+
+from aiohttp import web
+
+def index(request):
+	return web.Response(body=b'<h1>Lambda~</h1>')
+	
+async def init(loop):
+	app = web.Application(loop=loop)
+	app.route.add_route('GET','/',index)
+	srv = await loop.create_server(app.make_handler(), '127.0.0.1',9000)
+	logging.info('server start at http://127.0.0.1:9000...')
+	return srv
+	
+loop = asuncio.get_event_loop()
+loop.run_until_complete(init(loop))
+loop.run_forever()
